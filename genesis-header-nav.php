a863162eccd786cd81a7b3c6b9be17e56aa10ece
GaryJones <gary@garyjones.co.uk>
2013-09-07 23:18:27 +0100

    Add support for GitHub Updater plugin.

--- genesis-header-nav.php
+++ genesis-header-nav.php
@@ -9,16 +9,18 @@
  * @copyright 2013 Gary Jones, Gamajo Tech
  *
  * @wordpress-plugin
- * Plugin Name: Genesis Header Nav
- * Plugin URI:  https://github.com/GaryJones/genesis-header-nav
- * Description: Registers a menu location and displays it inside the header for a Genesis Framework child theme.
- * Version:     1.1.1
- * Author:      Gary Jones
- * Author URI:  http://gamajo.com/
- * Text Domain: genesis-header-nav
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * Domain Path: /languages
+ * Plugin Name:       Genesis Header Nav
+ * Plugin URI:        https://github.com/GaryJones/genesis-header-nav
+ * Description:       Registers a menu location and displays it inside the header for a Genesis Framework child theme.
+ * Version:           1.1.1
+ * Author:            Gary Jones
+ * Author URI:        http://gamajo.com/
+ * Text Domain:       genesis-header-nav
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/GaryJones/genesis-header-nav
+ * GitHub Branch:     master
  */
 
 // If this file is called directly, abort.
